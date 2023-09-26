99ae7b975595638d0ff3c96d72eb62f905e13289
Jonathan Cheng <jcheng461@gmail.com>
2023-09-26 13:53:56 -0700

    set up and pass test for package constructor
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- Parcels.Tests/ModelTests/PackageTest.cs
+++ Parcels.Tests/ModelTests/PackageTest.cs
@@ -12,8 +12,22 @@ namespace Parcels.Tests
     [TestMethod]
     public void PackageConstructor_CreatePackageInstance_Package()
     {
-      Package newPackage = new Package();
+      Package newPackage = new Package(5, 2, 8, 500);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
+    }
+
+    [TestMethod]
+    public void GetDimensions_CreatePackageInstanceWithParameters_Package()
+    {
+      int expectedLength = 5;
+      int expectedWidth = 2;
+      int expectedHeight = 8;
+      int expectedWeight = 500;
+      Package newPackage = new Package(5, 2, 8, 500);
+      Assert.AreEqual(expectedLength, newPackage.Length);
+      Assert.AreEqual(expectedWidth, newPackage.Width);
+      Assert.AreEqual(expectedHeight, newPackage.Height);
+      Assert.AreEqual(expectedWeight, newPackage.Weight);
     }
   }
 }
--- Parcels/Models/Package.cs
+++ Parcels/Models/Package.cs
@@ -4,9 +4,20 @@ namespace Parcels.Models
 {
   public class Package
   {
-    public static int Dimension { get; set; }
-    public static int Weight { get; set; }
+    // Package Dimensions
+    public int Length { get; } // how long
+    public int Width { get; } // how wide
+    public int Height { get; } // how tall
+    // Package Mass
+    public int Weight { get; } // how heavy
 
- 
+    // Package Instance Constructor
+    public Package(int pkgLength, int pkgWidth, int pkgHeight, int pkgWeight)
+    {
+      Length = pkgLength;
+      Width = pkgWidth;
+      Height = pkgHeight;
+      Weight = pkgWeight;
+    }
   }
 }
